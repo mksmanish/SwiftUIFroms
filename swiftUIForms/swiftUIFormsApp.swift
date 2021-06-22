871045d5b390ff7e2cbdf1d6f6866af5a9d60e6c
manish <manishk@winjit.com>
2021-06-22 11:41:02 +0530

    Initial Commit

---
+++ swiftUIForms/swiftUIFormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIFormsApp.swift
+//  swiftUIForms
+//
+//  Created by manish on 22/06/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIFormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
